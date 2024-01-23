eb1b7c7df27221ff590a1536268a737a3d5dd390
Cédric Vianney Mokoko Bayito <cedricbayito@gmail.com>
2024-01-23 22:35:08 +0100

    review footer

--- src/components/SearchContents/ResultsSearch/ResultsSearch.js
+++ src/components/SearchContents/ResultsSearch/ResultsSearch.js
@@ -20,33 +20,6 @@ const ResultsSearch = ({ contentsResults }) => {
           ))}
         </div>
       </div>
-      <div className={styles.footer}>
-        <div className={styles.link}>
-          <p>Condizioni generali di abbonnamento</p>
-          <p>Informazioni sulla privacy</p>
-          <p>Norma sulla privacy in UE e UK</p>
-          <p>Policy sui cookie</p>
-          <p>Dispositivi supportati</p>
-          <p>Assistenza</p>
-          <p>Chi siamo</p>
-          <p>Gestione preferenze dati personali</p>
-        </div>
-        <p className={`${styles.copyText}`}>
-          <FontAwesomeIcon
-            icon={faCopyright}
-            className={`${styles.copyRight}`}
-          />
-          <Link
-            href={`https://cedricmokoko.com/`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className={`${styles.copyText}`}>
-              Eyes_Motion by Cédric Mokoko
-            </span>
-          </Link>
-        </p>
-      </div>
     </div>
   );
 };
