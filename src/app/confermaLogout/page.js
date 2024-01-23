482943c9ba99d508c3efcc94e0ed0aa435eeeb03
Cédric Vianney Mokoko Bayito <cedricbayito@gmail.com>
2024-01-23 03:49:43 +0100

    pop pre logout

---
+++ src/app/confermaLogout/page.js
@@ -0,0 +1,47 @@
+"use client";
+import { redirect } from "next/navigation";
+import styles from "./pageConfermaLogout.module.scss";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+const LogoutPopUp = () => {
+  // const session = await getServerSession(authOptions);
+  // if (!session) {
+  // }
+  // Get the router instance
+  const router = useRouter();
+
+  // Handle the sign out button click
+  const handleSignOut = () => {
+    // You can perform any sign-out logic here
+    // ...
+
+    // Redirect to the confirmation logout page
+    router.push("/private/user");
+
+    // redirect("/private/user");
+  };
+
+  return (
+    <div className={styles.containerPopUp}>
+      <div className={styles.popUp}>
+        <div className={styles.question}>
+          <p>Sei sicuro di voler uscire dall'app</p>
+        </div>
+        <div className={styles.buttonContainer}>
+          <button className={styles.button} onClick={handleSignOut}>
+            <h2>No, rimango</h2>
+          </button>{" "}
+          <button
+            // data-tooltip="Are you sure you want to exit the application?"
+            className={`${styles.button}`}
+            onClick={() => signOut({ callbackUrl: "/" })}
+          >
+            <h2>si, esco</h2>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default LogoutPopUp;
--- src/components/SignOutButton/SignOutButton.js
+++ src/components/SignOutButton/SignOutButton.js
@@ -1,19 +1,52 @@
+// "use client";
+// // import { signOut } from "next-auth/react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
+// import styles from "./SignOutButton.module.scss";
+// import { redirect } from "next/dist/server/api-utils";
+
+// const SignOutButton = () => {
+//   return (
+//     <div className={`${styles.container}`}>
+//       <button
+//         // data-tooltip="Are you sure you want to exit the application?"
+//         className={`${styles.button}`}
+//         onClick={() => redirect("/confermaLogout")}
+//       >
+//         <h2>Sign out</h2>
+//         <FontAwesomeIcon icon={faRightToBracket} className={`${styles.icon}`} />
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default SignOutButton;
+
+// Import necessary dependencies
 "use client";
-import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import styles from "./SignOutButton.module.scss";
 
 const SignOutButton = () => {
+  // Get the router instance
+  const router = useRouter();
+
+  // Handle the sign out button click
+  const handleSignOut = () => {
+    // You can perform any sign-out logic here
+    // ...
+
+    // Redirect to the confirmation logout page
+    router.push("/confermaLogout");
+  };
+
   return (
-    <div className={`${styles.container}`}>
-      <button
-        // data-tooltip="Are you sure you want to exit the application?"
-        className={`${styles.button}`}
-        onClick={() => signOut({ callbackUrl: "/" })}
-      >
+    <div className={styles.container}>
+      <button className={styles.button} onClick={handleSignOut}>
         <h2>Sign out</h2>
-        <FontAwesomeIcon icon={faRightToBracket} className={`${styles.icon}`} />
+        <FontAwesomeIcon icon={faRightToBracket} className={styles.icon} />
       </button>
     </div>
   );
